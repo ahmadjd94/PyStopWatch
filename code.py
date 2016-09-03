eb182c915ae5c0110139c4fd49b4b80b7acb73ee
banana <ahmadjd94@bitbucket.org>
2016-09-04 01:25:00 +0300

    work in progress #todo : restore and continue session option add load rows functionality to table views

--- code.py
+++ code.py
@@ -13,32 +13,40 @@ from pymongo import MongoClient
 from threading import Thread ,Lock
 
 import time
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(400, 157)
+        MainWindow.resize(400, 499)
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.pushButton = QtWidgets.QPushButton(self.centralWidget)
-        self.pushButton.setGeometry(QtCore.QRect(310, 5, 80, 22))
+        self.pushButton.setGeometry(QtCore.QRect(290, 100, 80, 22))
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralWidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(310, 34, 80, 22))
+        self.pushButton_2.setGeometry(QtCore.QRect(290, 130, 80, 22))
         self.pushButton_2.setObjectName("pushButton_2")
         self.pushButton_3 = QtWidgets.QPushButton(self.centralWidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(310, 64, 80, 22))
+        self.pushButton_3.setGeometry(QtCore.QRect(290, 160, 80, 22))
         self.pushButton_3.setObjectName("pushButton_3")
         self.lcdNumber = QtWidgets.QLCDNumber(self.centralWidget)
-        self.lcdNumber.setGeometry(QtCore.QRect(0, 0, 311, 91))
-        self.lcdNumber.setInputMethodHints(QtCore.Qt.ImhDate | QtCore.Qt.ImhTime)
+        self.lcdNumber.setGeometry(QtCore.QRect(10, 0, 381, 91))
+        self.lcdNumber.setInputMethodHints(QtCore.Qt.ImhDate|QtCore.Qt.ImhTime)
         self.lcdNumber.setSmallDecimalPoint(False)
-        self.lcdNumber.setDigitCount(16)
+        self.lcdNumber.setDigitCount(8)
         self.lcdNumber.setProperty("intValue", 0)
         self.lcdNumber.setObjectName("lcdNumber")
+        self.label = QtWidgets.QLabel(self.centralWidget)
+        self.label.setGeometry(QtCore.QRect(20, 100, 91, 16))
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralWidget)
+        self.lineEdit.setGeometry(QtCore.QRect(120, 100, 113, 22))
+        self.lineEdit.setObjectName("lineEdit")
+        self.tableView = QtWidgets.QTableView(self.centralWidget)
+        self.tableView.setGeometry(QtCore.QRect(10, 200, 361, 241))
+        self.tableView.setObjectName("tableView")
         MainWindow.setCentralWidget(self.centralWidget)
         self.menuBar = QtWidgets.QMenuBar(MainWindow)
-        self.menuBar.setGeometry(QtCore.QRect(0, 0, 400, 20))
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 400, 19))
         self.menuBar.setObjectName("menuBar")
         MainWindow.setMenuBar(self.menuBar)
         self.mainToolBar = QtWidgets.QToolBar(MainWindow)
@@ -49,26 +57,22 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusBar)
         self.pushButton.clicked.connect(starter)
         self.pushButton_2.clicked.connect(stop)
+
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-
-
-
-
-
     def retranslateUi(self, MainWindow):
-        flag=True
-
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "START"))
         self.pushButton_2.setText(_translate("MainWindow", "STOP"))
         self.pushButton_3.setText(_translate("MainWindow", "PAUSE"))
+        self.label.setText(_translate("MainWindow", "session name"))
+
 
 def count():
 
-    while Flag.__flag__:
+    while Publics.__flag__:
             ThreadLock.acquire()
             ui.lcdNumber.display(str(datetime.datetime.now() - now).split('.')[0])
             # self.lcdNumber.display(str(datetime.datetime.now()-now))
@@ -77,8 +81,9 @@ def count():
     sessiontime =datetime.datetime.now()
     print ('exiting')
 
-class Flag :
+class Publics :
      __flag__=True
+     __now__=None
 def setup(function):
     if function=='stop':
         pass
@@ -86,25 +91,28 @@ def setup(function):
         pass
 
 def starter ():
+
     if not threads[0].is_alive():
-        Flag.__flag__=True
+        Publics.__now__ = datetime.datetime.now()
+        collection.log.insert ({'name':ui.lineEdit.text(),'starttime':Publics.__now__,'status':'current'})
+        ui.lineEdit.setEnabled(False)
+        Publics.__flag__=True
         threads[0].start()
     else :
         threads[0].run()
 def stop () :
     #threads[0].join()
     ThreadLock.acquire()
-    Flag.__flag__=False
+    collection.log.update({'starttime':Publics.__now__},{'status':'stopped'})
+    Publics.__flag__=False
 
     ThreadLock.release()
 if __name__ == "__main__":
 
     client =MongoClient() # initialize connection
-    a=client.log  #initialize connection
-
-
+    collection=client.log  #initialize connection
     ThreadLock= Lock()
-    now = datetime.datetime.now()
+
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
@@ -113,6 +121,13 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     threads = [Thread(target=count,daemon=True),Thread(target=stop,daemon=True)]
+    sets=collection.log.find()
+    row=sets.count()
+    print(row)
+    a=sets.next()
+    # while a :
+    #     ui.tableView.setCurrentIndex(a)
+    #     a = sets.next()
 
     # t2 = Thread(ui.stop, daemon=False)
     sys.exit(app.exec_())
